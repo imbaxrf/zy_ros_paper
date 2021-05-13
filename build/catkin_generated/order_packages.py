ad03a63abcc53746141ce5de3e6571c26fd1a771
creeperZY <869308717@qq.com>
2021-05-13 16:58:51 +0800

    add zy_ws

--- build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/root/zy_ros_paper/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/root/e100_ws/devel;/root/r100_ws/devel;/opt/ros/kinetic'.split(';') if '/root/e100_ws/devel;/root/r100_ws/devel;/opt/ros/kinetic' != '' else []
